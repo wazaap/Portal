2e024c4262810de6eb80d24fe5fe7ca005829501
Thomas Uldall Leonhard <thomas.leonhard@avanade.com>
2018-08-16 22:34:51 +0200

    DI member service agent added

--- Source/Web/Portal.Web/Controllers/MemberController.cs
+++ Source/Web/Portal.Web/Controllers/MemberController.cs
@@ -1,10 +1,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Portal.Services.MemberService;
 using Portal.Web.Models.Member;
 
 namespace Portal.Web.Controllers{
     public class MemberController: Controller{
+        private readonly IMemberServiceAgent memberServiceAgent;
+
+        public MemberController(IMemberServiceAgent memberServiceAgent)
+        {
+            this.memberServiceAgent = memberServiceAgent;
+        }
         public IEnumerable<MemberListModel> Index(){
             return Enumerable.Empty<MemberListModel>();
         }
--- Source/Web/Portal.Web/Startup.cs
+++ Source/Web/Portal.Web/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using Portal.Web.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Portal.Services.MemberService;
 
 namespace Portal.Web
 {
@@ -41,6 +42,8 @@ namespace Portal.Web
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddTransient<IMemberServiceAgent, MemberDummyServiceAgent>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
